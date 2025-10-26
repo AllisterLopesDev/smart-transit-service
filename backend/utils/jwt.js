92e352e0b794bc62d56257690fefa22cee56ca7a
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2025-10-26 17:52:09 +0530

    Refactor logging: replace console.log with logger in server.js and jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
+const logger = require("../utils/logger");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
-console.log("token expires in seconds:", JWT_EXPIRES_IN);
+logger.debug(`token expires in seconds: ${JWT_EXPIRES_IN}`);
 
 function generateToken(payload) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
