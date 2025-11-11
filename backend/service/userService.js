ed914984b2eb7dd69bf521a18d72eb4715c207dd
Girish Suroji <girishsuroji04@gmail.com>
2025-11-11 21:05:51 +0530

    Removed comments (userAService) & remove access token function (authController)

--- backend/service/userService.js
+++ backend/service/userService.js
@@ -1,13 +1,12 @@
  const pool = require("../db/index");
  
- // Check if user already exists
     exports.findUserByEmailOrPhone = async (email, full_phone) => {
     const existingUser = await pool.query(
       "SELECT * FROM users WHERE email = $1 OR full_phone = $2",
       [email, full_phone]
     );
 
-   // Return user if found, else null
+   
     return existingUser.rows[0] || null;
 
 };
