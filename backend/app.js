ee666eb5f42ac48f82aa5f7db04900338a5406bd
Girish Suroji <girishsuroji04@gmail.com>
2025-11-22 01:03:25 +0530

    removed comments

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,12 @@ const express = require("express");
 const app = express();
 
 // Routes
-// const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
-// const loginRouter = require("./routes/login");
-
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// // login route
-// app.use("/auth", loginRouter);
-
 // Auth route
-// app.use("/", indexRouter);
 app.use("/auth", authRouter);
 
 module.exports = app;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,15 +9,10 @@ const { registrationValidationRules } = require("../validation/authValidation");
 const { loginValidationRules } = require("../validation/authValidation");
 const { login } = require("../controllers/authController");
 
-
 //registration route
 router.post("/register", registrationValidationRules, register );
 
 //login route
-router.post("/login", loginValidationRules, login);
-
-// router.get("/protected", authMiddleware, (req, res) => {
-//   res.json(success({ user: req.user }, "Protected route accessed"));
-// });
+router.post("/login", loginValidationRules, login); 
 
 module.exports = router;
